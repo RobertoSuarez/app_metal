7b5e3a9925045c9113365e8783053ad55dc4ee40
Karla_Almea <kalmeav@uteq.edu.ec>
2023-01-12 21:29:01 -0500

    Validaciones de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Login } from './login.types';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   focus;
   focus1;
+
+  userLoginForm: FormGroup = new FormGroup({
+    correo: new FormControl('kalmeav@uteq.edu.ec', [Validators.required, Validators.email]),
+    contraseña: new FormControl('clave', [Validators.required, Validators.minLength(6) ]),
+  })
   constructor() { }
 
   ngOnInit() {
+  }
+  onLoginUsuario(){
+    console.log(this.userLoginForm.value);
   }
 
 }
---
+++ src/app/login/login.types.ts
@@ -0,0 +1,5 @@
+
+export interface Login {
+    correo: string;
+    contraseña: string;
+}
