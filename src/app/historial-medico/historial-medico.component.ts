162ffd85d32fb2c9f0217025d3cc603ba895a45b
RobertoSuarez <electrosonix12@gmail.com>
2023-02-24 19:09:20 -0500

    Modal para mostrar información de la cita

--- src/app/historial-medico/historial-medico.component.ts
+++ src/app/historial-medico/historial-medico.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CitasService } from '../core/cita/citas.service';
 import { HttpErrorResponse } from "@angular/common/http";
 import { Cita } from '../core/cita/citas.types';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-historial-medico',
@@ -10,11 +11,15 @@ import { Cita } from '../core/cita/citas.types';
   styleUrls: ['./historial-medico.component.css']
 })
 export class HistorialMedicoComponent implements OnInit {
+  
   err: boolean= false;
+
   citasPresentar: Cita[];
+
   citaForm: FormGroup = new FormGroup({
-    cedula: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    cedula: new FormControl('1206773572', [Validators.required, Validators.minLength(10)]),
   })
+
   constructor(
     private citasService: CitasService
   ) { }
@@ -43,6 +48,41 @@ export class HistorialMedicoComponent implements OnInit {
         this.err = true;
       }
     });
+  }
+
+  mostrarCita(cita: Cita) {
+    Swal.fire({
+      title: 'Información de la cita médica',
+      html: `
+        <div class="border border-primary text-left p-2">
+          <div>
+            ID: ${cita.id}
+          </div>
+          <div>
+            Paciente: ${cita.paciente.nombres} ${cita.paciente.apellidos}
+          </div>
+          <div>
+            Cédula: ${cita.paciente.cedula}
+          </div>
+          <div>
+            Correo: ${cita.paciente.correo}
+          </div>
+          <div>
+            Doctor: ${cita.doctor.nombres} ${cita.doctor.apellidos}
+          </div>
+          <div>
+            Fecha: ${cita.fecha}
+          </div>
+          <div>
+            Descripción: ${cita.descripcion}
+          </div>
+          
+        </div>
+      `,
+      showCancelButton: false,
+      showConfirmButton: true,
+      confirmButtonText: "Aceptar"
+    })
   }
 
 }
